38067d0bc98fd378a4e1f510cfacda0fab5df7f2
bom_d_van <bom.d.van@gmail.com>
2013-02-22 22:53:25 +0800

    simple forwarding now is processed correctly

--- document_match.go
+++ document_match.go
@@ -2,23 +2,43 @@ package mailthread
 
 import (
     // "regexp"
-    // "fmt"
+    "fmt"
     "strings"
 )
 
 func Process(input string) (output string, err error) {
-    inputSlice := strings.Split(input, "\n")
+    inputSlice := strings.SplitAfter(input, "\n")
+    endTagCounter := 1
+    // * remove `\n` match in simple_forward.go
+    // * endTagStack
+    // * output
+        
+    // append top tag
+    // push outter end tag into endTagStack
+    // ... process
+    // pop end tag
     
+    output += "<div class=\"message\">\n"
+    insideForWardBlock := false
     for _, line := range inputSlice {
         switch {
         case isForwardingBlockStart(line):
-            
-        case isForwardingBlockEnd(line):
-            
+            output += "<div class=\"forwarded_message_header\">\n"
+            output += line
+            insideForWardBlock = true
+        case insideForWardBlock && isForwardingBlockEnd(line):
+            output += line
+            output += "</div>\n"
+            insideForWardBlock = false
         case isReplyLine(line):
-            
-        } 
+            endTagCounter += 1
+            output += fmt.Sprintf("<div class=\"reply\"><div class=\"reply_header\">\n%s\n</div>\n", line)
+        default:
+            output += line
+        }
     }
+    
+    output += strings.Repeat("\n</div>\n", endTagCounter)
     
     return
 }
