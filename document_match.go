6136108781522c37bd9c2cd394e56779b3278557
bom_d_van <bom.d.van@gmail.com>
2013-02-22 16:26:40 +0800

    fixing stupid submitted-mistake and adding a simple control flow, both in document_match.go

--- document_match.go
+++ document_match.go
@@ -1,6 +1,24 @@
 package mailthread
 
 import (
-    "regexp"
-    "fmt"
+    // "regexp"
+    // "fmt"
+    "strings"
 )
+
+func Process(input string) (output string, err error) {
+    inputSlice := strings.Split(input, "\n")
+    
+    for _, line := range inputSlice {
+        switch {
+        case isForwardingBlockStart(line):
+            
+        case isForwardingBlockEnd(line):
+            
+        case isReplyLine(line):
+            
+        } 
+    }
+    
+    return
+}
